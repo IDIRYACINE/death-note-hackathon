f494adb457404b72d7ece0396c7645fae291cc9f
IDIRYACINE <idiryacinesp@gmail.com>
2023-09-07 13:44:39 +0000

    init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
+import ConvexClientProvider from '@/components/ConvexClientProvider'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ConvexClientProvider>
+      <Component {...pageProps} />
+    </ConvexClientProvider>
+  )
 }
