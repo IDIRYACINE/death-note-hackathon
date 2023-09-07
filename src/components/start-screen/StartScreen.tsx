b323efcdde1ed59efbd0b6dd0843622375e90377
IDIRYACINE <idiryacinesp@gmail.com>
2023-09-07 14:00:18 +0000

    setup

---
+++ src/components/start-screen/StartScreen.tsx
@@ -0,0 +1,10 @@
+
+
+export default function StartScreen(){
+
+
+    return (
+        <div>
+        </div>
+    )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import ConvexClientProvider from '@/components/ConvexClientProvider'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import React from 'react';
+import { ConfigProvider } from 'antd';
+import theme from '@/theme/ThemeConfig';
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ConvexClientProvider>
-      <Component {...pageProps} />
+      <ConfigProvider theme={theme}>
+        <Component {...pageProps} />
+      </ConfigProvider>
     </ConvexClientProvider>
   )
 }
